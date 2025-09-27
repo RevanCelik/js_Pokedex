b61b00eb11139d1fe424b2d40da6b07cbbbe52ee
Revan Celik <revancelik444@gmail.com>
2025-09-27 08:12:32 +0200

    content added

---
+++ script/db.js
@@ -0,0 +1,34 @@
+async function fetchDataJson() {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=0');
+    let responseAsJson = await response.json();
+    let pokemons = responseAsJson.results;
+
+    renderPokemon(pokemons);
+}
+
+async function renderPokemon(pokemons) {
+    let pokemonContainer = document.getElementById("pokemon-content");
+    pokemonContainer.innerHTML = "";
+
+    for (let i = 0; i < pokemons.length; i++) {
+        let pokemon = pokemons[i];
+        let imageResponse = await fetch(pokemon.url);
+        let imageDetail = await imageResponse.json();
+
+        pokemonContainer.innerHTML += `
+            <div>
+                <div>
+                    <strong>Name: ${pokemon.name}</strong>
+                </div>
+                <div>
+                <img src="${imageDetail.sprites.front_default}">
+                </div>
+            </div>    
+                `;
+    }
+}
+
+
+
+
+// Second 30: https://pokeapi.co/api/v2/pokemon?limit=30&offset=30
